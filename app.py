d0df8376949bcce5ca12af9754091df0e67b00d7
Yuichi Motohashi <bookbridge.y@gmail.com>
2017-02-22 23:45:43 +0900

    revert

--- app.py
+++ app.py
@@ -82,52 +82,28 @@ def callback():
 def handle_text_message(event):
     text = event.message.text
     if text == "all":
-        rss = feedparser.parse("http://b.hatena.ne.jp/hotentry?mode=rss")
+        rss = feedparser.parse("http://b.hatena.ne.jp/hotentry?mode=rss&of=5")
         carousel_template = CarouselTemplate(columns=[
-            CarouselColumn(text=rss.entries[0].link, title=rss.entries[0].title, actions=[
-                URITemplateAction(label='Go to this page',
-                                  uri=rss.entries[0].link)]),
-            CarouselColumn(text=rss.entries[1].link, title=rss.entries[1].title, actions=[
-                URITemplateAction(label='Go to this page',
-                                  uri=rss.entries[1].link)]),
-        
+            CarouselColumn(text=rss.entries[0].link,
+                           title=rss.entries[0].title,
+                           actions=[URITemplateAction(label='Go to this page',
+                                                      uri=rss.entries[0].link)]),
+            CarouselColumn(text=rss.entries[1].link,
+                           title=rss.entries[1].title,
+                           actions=[URITemplateAction(label='Go to this page',
+                                                      uri=rss.entries[1].link)]),
         ])
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=carousel_template)
         line_bot_api.reply_message(event.reply_token, template_message)
 
-    elif text in ["social", "economics", "life", "knowledge", "it", "fun", "entertainment", "game"]:
+    elif text in ["social", "economics", "life", "knowledge",
+                  "it", "fun", "entertainment", "game"]:
         pass
     else:
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage(text=event.message.text))
 
-
-def generate_carousel(rss):
-    print("generate_carousel called")
-    carousel_template = CarouselTemplate(columns=[
-        CarouselColumn(text=rss.entries[0].link,
-                       title=rss.entries[0].title,
-                       actions=[URITemplateAction(label='Go to this page',
-                                                  uri=rss.entries[0].link)]),
-        CarouselColumn(text=rss.entries[1].link,
-                       title=rss.entries[1].title,
-                       actions=[URITemplateAction(label='Go to this page',
-                                                  uri=rss.entries[1].link)]),
-        CarouselColumn(text=rss.entries[2].link,
-                       title=rss.entries[2].title,
-                       actions=[URITemplateAction(label='Go to this page',
-                                                  uri=rss.entries[2].link)]),
-        CarouselColumn(text=rss.entries[3].link,
-                       title=rss.entries[3].title,
-                       actions=[URITemplateAction(label='Go to this page',
-                                                  uri=rss.entries[3].link)]),
-        CarouselColumn(text=rss.entries[4].link,
-                       title=rss.entries[4].title,
-                       actions=[URITemplateAction(label='Go to this page',
-                                                  uri=rss.entries[4].link)]),
-    ])
-    return carousel_template
 
 
 if __name__ == "__main__":
