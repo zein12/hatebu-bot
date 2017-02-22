ffebecea7ee8351e41bb74e0f1832593f50ae8c3
Yuichi Motohashi <bookbridge.y@gmail.com>
2017-02-22 23:49:23 +0900

    add more carousels

--- app.py
+++ app.py
@@ -92,6 +92,15 @@ def handle_text_message(event):
                            title=rss.entries[1].link,
                            actions=[URITemplateAction(label='Go to this page',
                                                       uri=rss.entries[1].link)]),
+            CarouselColumn(text=rss.entries[2].title,
+                           title=rss.entries[2].link,
+                           actions=[URITemplateAction(label='Go to this page',
+                                                      uri=rss.entries[2].link)]),
+            CarouselColumn(text=rss.entries[3].title,
+                           title=rss.entries[3].link,
+                           actions=[URITemplateAction(label='Go to this page',
+                                                      uri=rss.entries[3].link)]),
+
         ])
         template_message = TemplateSendMessage(
             alt_text='Buttons alt text', template=carousel_template)
